f10efa3f3a7fc1a8500e04924e38ba328d9ef650
vasily84 <35914841+vasily84@users.noreply.github.com>
2025-03-11 20:23:21 +0300

    Add files via upload

--- SIT_Source/source/MBTY/STD_LIB_NEW/Unit_TFromTable2dNew.pas
+++ SIT_Source/source/MBTY/STD_LIB_NEW/Unit_TFromTable2dNew.pas
@@ -2,7 +2,7 @@
 
 interface
 uses Classes, MBTYArrays, DataTypes, SysUtils, abstract_im_interface, RunObjts,
-  math, tbls, mbty_std_consts,Data_blocks;
+  math, tbls, mbty_std_consts, Data_blocks, TExtArray_Utils;
 
 //Двумерная линейная интерполяция по таблице из файла
  type
@@ -10,11 +10,17 @@ uses Classes, MBTYArrays, DataTypes, SysUtils, abstract_im_interface, RunObjts,
   protected
     table:         TTable2;
   public
-    filename:      string;
+    fileName,fileNameArgs,fileNameVals:      string;
     interp_method: NativeInt;
     extrType: NativeInt;
     inputType: NativeInt;
 
+    prop_X1_arr, prop_X2_arr: TExtArray;
+    prop_DataTable: TExtArray2;
+
+    function LoadDataFromPorts(): Boolean;
+    function LoadDataFromProperties(): Boolean;
+    function LoadDataFromFiles12():Boolean;
     function LoadDataFromFile(): Boolean;
     function LoadData(): Boolean;
     function checkXY_Range(var aX1,aX2: RealType;var aZvalue: RealType): Boolean;
@@ -32,6 +38,15 @@ uses Classes, MBTYArrays, DataTypes, SysUtils, abstract_im_interface, RunObjts,
 implementation
 uses RealArrays;
 
+const
+  // назначения входных портов
+  ROW_ARG = 0;
+  COL_ARG = 1;
+  ROW_ARGS_ARR = 2;
+  COL_ARGS_ARR = 3;
+  FUNCS_TABLE = 4;
+
+
 {*******************************************************************************
             Двумерная линейная интерполяция по таблице из файла
 *******************************************************************************}
@@ -40,12 +55,18 @@ begin
   inherited;
   table:=TTable2.Create('');
   interp_method:=0;
+  prop_X1_arr:= TExtArray.Create(1);
+  prop_X2_arr:= TExtArray.Create(1);
+  prop_DataTable:= TExtArray2.Create(1,1);
 end;
 
 destructor TFromTable2DNew.Destroy;
 begin
+  FreeAndNil(table);
+  FreeAndNil(prop_X1_arr);
+  FreeAndNil(prop_X2_arr);
+  FreeAndNil(prop_DataTable);
   inherited;
-  table.Free;
 end;
 
 function TFromTable2DNew.GetParamID;
@@ -53,6 +74,23 @@ begin
   Result:=inherited GetParamID(ParamName,DataType,IsConst);
   if Result <> -1 then exit;
 
+  if StrEqu(ParamName,'Row_arr') then begin
+    Result:=NativeInt(prop_X1_arr);
+    DataType:=dtDoubleArray;
+    exit;
+    end;
+
+  if StrEqu(ParamName,'Col_arr') then begin
+    Result:=NativeInt(prop_X2_arr);
+    DataType:=dtDoubleArray;
+    exit;
+    end;
+
+  if StrEqu(ParamName,'DataMatrix') then begin
+    Result:=NativeInt(prop_DataTable);
+    DataType:=dtMatrix;
+    exit;
+    end;
   if StrEqu(ParamName,'interp_method') then begin
     Result:=NativeInt(@interp_method);
     DataType:=dtInteger;
@@ -69,9 +107,19 @@ begin
     end;
 
   if StrEqu(ParamName,'filename') then begin
-    Result:=NativeInt(@filename);
+    Result:=NativeInt(@fileName);
     DataType:=dtString;
     end;
+
+  if StrEqu(ParamName,'fileNameArgs') then begin
+    Result:=NativeInt(@fileNameArgs);
+    DataType:=dtString;
+    end;
+if StrEqu(ParamName,'fileNameVals') then begin
+    Result:=NativeInt(@fileNameVals);
+    DataType:=dtString;
+    end;
+
 end;
 
 //----------------------------------------------------------------------------
@@ -139,20 +187,93 @@ begin
   // table.GetFunValueWithoutExtrapolation
 end;
 //----------------------------------------------------------------------------
+function TFromTable2DNew.LoadDataFromPorts(): Boolean;
+var
+  i,j,k,M,N: Integer;
+  f: RealType;
+begin
+  N := U[ROW_ARGS_ARR].Count;
+  M := U[COL_ARGS_ARR].Count;
+  // TODO - проверка размерности
+  if M*N<>U[FUNCS_TABLE].Count then begin
+    Result:=False;
+    exit;
+    end;
+
+  table.Arg1Count := N;
+  table.Arg2Count := M;
+
+  TExtArray_cpy(table.px1, U[ROW_ARGS_ARR]);
+  TExtArray_cpy(table.px2, U[COL_ARGS_ARR]);
+
+  table.py.ChangeCount(N,M);
+  k:=0;
+  for i:=0 to N-1 do begin
+    for j:=0 to M-1 do begin
+      f := U[FUNCS_TABLE].Arr[k];
+      Inc(k);
+      table.py[i][j]:=f;
+      end;
+    end;
+
+  Result := True;
+end;
+//----------------------------------------------------------------------------
+function TFromTable2DNew.LoadDataFromProperties(): Boolean;
+var
+  b1,b2: Boolean;
+begin
+  b1 := (prop_X1_arr.Count <> prop_DataTable.CountX);
+  b2:=  (prop_X2_arr.Count <> prop_DataTable.GetMaxCountY);
+
+  if (b1 or b2) then begin // некорректные размерности
+    Result:=False;
+    exit;
+    end;
+
+  table.Arg1Count := prop_X1_arr.Count;
+  table.Arg2Count := prop_X2_arr.Count;
+
+  TExtArray_cpy(table.px1, prop_X1_arr);
+  TExtArray_cpy(table.px2, prop_X2_arr);
+  TExtArray2_cpy(table.py, prop_DataTable);
+
+  Result := True;
+end;
+//----------------------------------------------------------------------------
+function TFromTable2DNew.LoadDataFromFiles12(): Boolean;
+begin
+  Result := False;
+end;
+//----------------------------------------------------------------------------
 function TFromTable2DNew.LoadDataFromFile(): Boolean;
 begin
   //Загрузка данных из файла с таблицей
   Result:=True;
-  table.OpenFromFile(FileName);
+  table.OpenFromFile(fileName);
   if (table.px1.Count = 0) or (table.px2.Count = 0) then begin
     Result:=False;
     exit;
-  end;
+    end;
 end;
 //----------------------------------------------------------------------------
 function TFromTable2DNew.LoadData(): Boolean;
 begin
-  Result := LoadDataFromFile();
+case inputType of
+  0:// ввести вручную через свойства
+    Result:=LoadDataFromProperties();
+  1:// в разных файлах
+    Result:=LoadDataFromFiles12();
+  2:// в одном файле
+    Result:=LoadDataFromFile();
+  3:// через порты
+    Result:=LoadDataFromPorts();
+  else
+    Assert(False,'unknown input type');
+    Result:=False;
+    exit;
+  end;
+
 end;
 //----------------------------------------------------------------------------
 function TFromTable2DNew.RunFunc(var at,h : RealType;Action:Integer):NativeInt;
@@ -162,61 +283,67 @@ begin
   Result:=r_Success;
 
   case Action of
-    f_InitObjects: begin
-                     //Загрузка данных из файла с таблицей
-                     if not LoadData() then begin
-                       ErrorEvent(txtErrorReadTable,msError,VisualObject);
-                       Result:=r_Fail;  //Если возвращаем > 0 - то значит произошла ошибка
-                     end;
-                   end;
+    f_InitObjects:
+      begin
+       //Загрузка данных из файла с таблицей
+       if not LoadData() then begin
+         ErrorEvent(txtErrorReadTable,msError,VisualObject);
+         Result:=r_Fail;
+       end;
+      end;
     f_UpdateJacoby,
     f_InitState,
     f_UpdateOuts,
     f_RestoreOuts,
     f_GoodStep:
-              case interp_method of
+      begin
+        //TODO - Загрузка данных из порта - почему сразу не выставляет данные на порту, неведомо
+        if inputType=3 then LoadDataFromPorts();
 
-                    0:  // линейная
-                      begin
-                      for i:=0 to U[0].Count - 1 do
-                        if checkXY_Range(U[0].Arr^[i],U[1].Arr^[i],Yval) then begin
-                          Y[0].Arr^[i]:=table.GetFunValue(U[0].Arr^[i],U[1].Arr^[i]); end
-                          else begin
-                          Y[0].Arr^[i]:=Yval; end;
-                      end;
+        case interp_method of
 
-                    1:  // кусочно-постоянная
-                       begin
-                         for i:=0 to U[0].Count - 1 do
-                          if checkXY_Range(U[0].Arr^[i],U[1].Arr^[i],Yval) then begin
-                            Y[0].Arr^[i]:=table.GetFunValueWithoutInterpolation(U[0].Arr^[i],U[1].Arr^[i]); end
-                            else begin
-                            Y[0].Arr^[i]:=Yval;
-                            end;
-                       end;
-
-                    2: // сплайны
-                      begin
-                          for i:=0 to U[0].Count - 1 do
-                            if checkXY_Range(U[0].Arr^[i],U[1].Arr^[i],Yval) then begin
-                            Y[0].Arr^[i]:=table.GetFunValueBySplineInterpolation(U[0].Arr^[i],U[1].Arr^[i]); end
-                            else begin
-                            Y[0].Arr^[i]:=Yval;
-                            end;
-                      end;
-
-                    3: // Акима
-                      begin
-                          for i:=0 to U[0].Count - 1 do
-                            if checkXY_Range(U[0].Arr^[i],U[1].Arr^[i],Yval) then begin
-                              Y[0].Arr^[i]:=table.GetFunValueByAkimaInterpolation(U[0].Arr^[i],U[1].Arr^[i]); end
-                              else begin
-                              Y[0].Arr^[i]:=Yval;
-                              end;
-                      end;
-                  else
-                    Assert(False,'Table2d interpolation method not implemented');
+            0:  // линейная
+              begin
+              for i:=0 to U[0].Count - 1 do
+                if checkXY_Range(U[0].Arr^[i],U[1].Arr^[i],Yval) then begin
+                  Y[0].Arr^[i]:=table.GetFunValue(U[0].Arr^[i],U[1].Arr^[i]); end
+                  else begin
+                  Y[0].Arr^[i]:=Yval; end;
               end;
+
+            1:  // кусочно-постоянная
+               begin
+                 for i:=0 to U[0].Count - 1 do
+                  if checkXY_Range(U[0].Arr^[i],U[1].Arr^[i],Yval) then begin
+                    Y[0].Arr^[i]:=table.GetFunValueWithoutInterpolation(U[0].Arr^[i],U[1].Arr^[i]); end
+                    else begin
+                    Y[0].Arr^[i]:=Yval;
+                    end;
+               end;
+
+            2: // сплайны
+              begin
+                  for i:=0 to U[0].Count - 1 do
+                    if checkXY_Range(U[0].Arr^[i],U[1].Arr^[i],Yval) then begin
+                    Y[0].Arr^[i]:=table.GetFunValueBySplineInterpolation(U[0].Arr^[i],U[1].Arr^[i]); end
+                    else begin
+                    Y[0].Arr^[i]:=Yval;
+                    end;
+              end;
+
+            3: // Акима
+              begin
+                  for i:=0 to U[0].Count - 1 do
+                    if checkXY_Range(U[0].Arr^[i],U[1].Arr^[i],Yval) then begin
+                      Y[0].Arr^[i]:=table.GetFunValueByAkimaInterpolation(U[0].Arr^[i],U[1].Arr^[i]); end
+                      else begin
+                      Y[0].Arr^[i]:=Yval;
+                      end;
+              end;
+          else
+            Assert(False,'Table2d interpolation method not implemented');
+        end;
+      end;
 
   end
 end;
@@ -244,7 +371,7 @@ begin
 end;
 //----------------------------------------------------------------------------
 function TFromTable2DNew.ReadParam(ID: NativeInt;ParamType:TDataType;DestData: Pointer;DestDataType: TDataType;MoveData:TMoveProc):boolean;
- var i: integer;
+// var i: integer;
 begin
   Result:=inherited ReadParam(ID,ParamType,DestData,DestDataType,MoveData);
   if not Result then
